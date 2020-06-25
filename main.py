1bf2e10a0199184ca7b031da1ad3d45bf31eb9d3
MusawerAli <pakjalihouse@gmail.com>
2020-06-24 23:04:22 -0500

    selenium automate

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ from selenium import webdriver
 from time import sleep
 
 class Scrappy:
-    def __init__(self,website):
+    def __init__(self,keyword):
         self.driver  = webdriver.Chrome()
-        self.driver.get("https://"+website)
+        # self.driver.get("https://"+website)
+        self.driver.get("https://www.airbnb.com/s/"+keyword+"/homes?tab_id=all_tab&refinement_paths%5B%5D=%2Fhomes&source=structured_search_input_header&search_type=search_query")
         sleep(5)
-        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[1]/div[1]/div/div[3]/div/div/div/div/form/div/div[1]/div[1]/div/div/label').send_keys('dubai')
-        sleep(1)
-        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[1]/div[1]/div/div[3]/div/div/div/div/form/div/div[2]/button/span[1]/span').click()
-
-        sleep(10)
-Scrappy("airbnb.com")
+        self.driver.find_element_by_xpath('/html/body/div[4]/div/div/div/div[1]/main/div/div/div/div[3]/aside/div/div[2]/div/button').click()
+        sleep(2)
+#  self.driver.find_element_by_xpath('//button[@data-testid="structured-search-input-search-button"]').click()
+        # self.driver.findElement(By.data-testid("structured-search-input-search-button")).click()
+        sleep(5)
+        data = self.driver.find_element_by_xpath("/html/body/div[4]/div/div/div/div[1]/main/div/div/div/div[1]/div[2]/div/div[2]/div/div/div[2]/div/div/div/div").text
+        print(data)
+        sleep(20)
+Scrappy("dubai")
