df303df077f256e67213613f8c2a065c0879561a
MusawerAli <pakjalihouse@gmail.com>
2020-06-27 14:44:53 -0500

    add links

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
-# scrappy
+# Run file by typing command
+# scrappy crawl quotes
+
+# Generating quotes.csv file by this command
+# scrapy crawl quotes -o quotes.csv
--- revzilla/spiders/scrap.py
+++
@@ -1,7 +0,0 @@
-import csv
-
-bins = [1,2,3,4,5,]
-frequencies = [232,232,3123,14,21]
-with open('some.csv', 'w') as f:
-    writer = csv.writer(f)
-    writer.writerows(zip(bins, frequencies))
