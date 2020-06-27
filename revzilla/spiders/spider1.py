931598e8050e7f3e44200b7240bd9f8dcc1dde3e
MusawerAli <pakjalihouse@gmail.com>
2020-06-27 14:44:11 -0500

    links

--- revzilla/spiders/spider1.py
+++ revzilla/spiders/spider1.py
@@ -11,14 +11,16 @@ class QuotesSpider(scrapy.Spider):
     def parse(self, response):
         questions = response.xpath('//div[@class="wysiwyg__content"]/section/h2/text()').getall()
         answers = response.xpath('//div[@class="wysiwyg__content"]/section/p/text()').getall()
+        links = response.xpath('//div[@class="wysiwyg__content"]/section/p/a').getall() 
         yield {
             'Questions': questions,
-            'Answers':answers
+            'Links'     :   links,
+            'Answers':answers,
 
         }
         with open('revzilla.csv', 'w') as f:
             writer = csv.writer(f)
-            writer.writerows(zip(questions, answers))
+            writer.writerows(zip(questions, links,answers))
         
 
 
